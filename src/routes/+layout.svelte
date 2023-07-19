e83c28258ec4eaa736d41c6e7e0fda733d0ecab0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-07-19 05:11:43 +0200

    add some more stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
-<nav>
-    <li><a href="/">markdoc page</a></li>
-    <li><a href="/svelte">svelte page</a></li>
-</nav>
-
 <main>
+    <nav style="margin-bottom: 0">
+        <ul>
+            <li><a href="/">markdoc page</a></li>
+            <li><a href="/svelte">svelte page</a></li>
+        </ul>
+    </nav>
     <slot />
 </main>
