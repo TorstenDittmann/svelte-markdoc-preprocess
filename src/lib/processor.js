460df23bde9c880de02c59fc9f25c74df9d31c6b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-07-19 04:53:56 +0200

    push readme

--- src/lib/processor.js
+++ src/lib/processor.js
@@ -38,7 +38,7 @@ export default (
 
             const code = transformer({
                 content,
-                options: {},
+                options,
                 tags
             });
 
