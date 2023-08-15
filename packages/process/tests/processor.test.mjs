db89a1537e9980830d035b9e9dcebd774ad4d3d2
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-08-16 00:01:48 +0200

    run formatter

--- packages/process/tests/processor.test.mjs
+++ packages/process/tests/processor.test.mjs
@@ -1,8 +1,8 @@
-import { test } from "node:test";
-import assert from "node:assert/strict";
-import { markdoc } from "../dist/module.js";
+import { test } from 'node:test';
+import assert from 'node:assert/strict';
+import { markdoc } from '../dist/module.js';
 
-test("preprocessor", async (context) => {
+test('preprocessor', async (context) => {
     const preprocess = markdoc();
 
     await context.test('should return a preprocessor', async () => {
@@ -10,35 +10,46 @@ test("preprocessor", async (context) => {
         assert.ok('markup' in preprocess);
     });
     await context.test('ignores non-extension files', async () => {
-        assert.equal(await preprocess.markup({
-            content: '# Hello World',
-            filename: 'test.svelte'
-        }), undefined);
+        assert.equal(
+            await preprocess.markup({
+                content: '# Hello World',
+                filename: 'test.svelte',
+            }),
+            undefined,
+        );
     });
 
     await context.test('handles extension files', async () => {
-        assert.deepEqual(await preprocess.markup({
-            content: '# Hello World',
-            filename: 'test.markdoc'
-        }), {
-            code: '<article><h1>Hello World</h1></article>'
-        });
+        assert.deepEqual(
+            await preprocess.markup({
+                content: '# Hello World',
+                filename: 'test.markdoc',
+            }),
+            {
+                code: '<article><h1>Hello World</h1></article>',
+            },
+        );
     });
 
     await context.test('respects config extensions', async () => {
         const preprocess = markdoc({
-            extensions: ['.test']
+            extensions: ['.test'],
         });
-        assert.equal(await preprocess.markup({
-            content: '# Hello World',
-            filename: 'test.markdoc'
-        }), undefined);
-        assert.deepEqual(await preprocess.markup({
-            content: '# Hello World',
-            filename: 'test.test'
-        }), {
-            code: '<article><h1>Hello World</h1></article>'
-        });
+        assert.equal(
+            await preprocess.markup({
+                content: '# Hello World',
+                filename: 'test.markdoc',
+            }),
+            undefined,
+        );
+        assert.deepEqual(
+            await preprocess.markup({
+                content: '# Hello World',
+                filename: 'test.test',
+            }),
+            {
+                code: '<article><h1>Hello World</h1></article>',
+            },
+        );
     });
-
 });
--- packages/process/tests/transformer.test.mjs
+++ packages/process/tests/transformer.test.mjs
@@ -1,59 +1,62 @@
-import { test } from "node:test";
-import assert from "node:assert/strict";
-import { sanitizeForSvelte, tsToType } from "../dist/transformer.js";
+import { test } from 'node:test';
+import assert from 'node:assert/strict';
+import { sanitizeForSvelte, tsToType } from '../dist/transformer.js';
 
-test("tsToType", async (context) => {
+test('tsToType', async (context) => {
     const node = {
-        type: "BindingIdentifier",
-    }
-    await context.test("handles strings", () => {
+        type: 'BindingIdentifier',
+    };
+    await context.test('handles strings', () => {
         const type = tsToType({
             ...node,
             typeAnnotation: {
                 typeAnnotation: {
-                    type: "TsKeywordType",
-                    kind: "string"
-                }
-            }
+                    type: 'TsKeywordType',
+                    kind: 'string',
+                },
+            },
         });
         assert.equal(type, String);
     });
-    await context.test("handles numbers", () => {
+    await context.test('handles numbers', () => {
         const type = tsToType({
             ...node,
             typeAnnotation: {
                 typeAnnotation: {
-                    type: "TsKeywordType",
-                    kind: "number"
-                }
-            }
+                    type: 'TsKeywordType',
+                    kind: 'number',
+                },
+            },
         });
         assert.equal(type, Number);
     });
-    await context.test("handles boolean", () => {
+    await context.test('handles boolean', () => {
         const type = tsToType({
             ...node,
             typeAnnotation: {
                 typeAnnotation: {
-                    type: "TsKeywordType",
-                    kind: "boolean"
-                }
-            }
+                    type: 'TsKeywordType',
+                    kind: 'boolean',
+                },
+            },
         });
         assert.equal(type, Boolean);
     });
-    await context.test("falls back to string", () => {
+    await context.test('falls back to string', () => {
         const type = tsToType(node);
         assert.equal(type, String);
     });
 });
 
-test("sanitize for svelte", async (context) => {
+test('sanitize for svelte', async (context) => {
     await Promise.all(
-        ['{', '}'].map(async (char) => context.test(`takes care of ${char}`, () => {
-            const sanitized = sanitizeForSvelte(char);
-            assert.ok(!sanitized.includes(char));
-            assert.ok(sanitized.startsWith('&'));
-            assert.ok(sanitized.endsWith(';'));
-        })));
+        ['{', '}'].map(async (char) =>
+            context.test(`takes care of ${char}`, () => {
+                const sanitized = sanitizeForSvelte(char);
+                assert.ok(!sanitized.includes(char));
+                assert.ok(sanitized.startsWith('&'));
+                assert.ok(sanitized.endsWith(';'));
+            }),
+        ),
+    );
 });
