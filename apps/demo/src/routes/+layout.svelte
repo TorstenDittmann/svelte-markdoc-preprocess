a8b0595d8b774dd8a3168f8d9575abae91156bf0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-08-17 00:28:05 +0200

    add github link

--- apps/demo/src/routes/+layout.svelte
+++ apps/demo/src/routes/+layout.svelte
@@ -7,6 +7,7 @@
         <h1>svelte-markdoc-preprocess</h1>
         <a href="/">home</a> /
         <a href="/docs">docs</a>
+        <a target="_blank" href="https://github.com/TorstenDittmann/svelte-markdoc-preprocess">github</a>
 		{#if dev}
 			/ <a href="/playground">playground</a>
 		{/if}
