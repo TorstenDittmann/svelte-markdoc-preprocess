4097216c556aec7a4f7a9f4322ee661ff8f0b912
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-08-20 01:21:24 +0200

    fix: navigation divider

--- apps/demo/src/routes/+layout.svelte
+++ apps/demo/src/routes/+layout.svelte
@@ -5,15 +5,15 @@
 <header>
     <nav>
         <h1>svelte-markdoc-preprocess</h1>
-        <a href="/">home</a> /
-        <a href="/install">install</a> /
-        <a href="/nodes">nodes</a> /
-        <a href="/tags">tags</a> /
-        <a href="/layouts">layouts</a> /
-        <a href="/advanced">advanced</a> /
+        <a href="/">home</a> |
+        <a href="/install">install</a> |
+        <a href="/nodes">nodes</a> |
+        <a href="/tags">tags</a> |
+        <a href="/layouts">layouts</a> |
+        <a href="/advanced">advanced</a> |
         <a target="_blank" href="https://github.com/TorstenDittmann/svelte-markdoc-preprocess">github</a>
 		{#if dev}
-			/ <a href="/playground">playground</a>
+			| <a href="/playground">playground</a>
 		{/if}
     </nav>
 </header>
