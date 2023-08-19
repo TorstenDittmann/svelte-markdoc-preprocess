fbb10763e318056c9733c37cced43049aa9c901b
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-08-20 01:15:30 +0200

    fix: optimize fence component

--- apps/demo/src/lib/Fence.svelte
+++ apps/demo/src/lib/Fence.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
     import 'highlight.js/styles/github-dark.css';
-    import hljs from 'highlight.js';
+    import hljs from 'highlight.js/lib/core';
+    import javascript from 'highlight.js/lib/languages/javascript';
+    import xml from 'highlight.js/lib/languages/xml';
+    import shell from 'highlight.js/lib/languages/shell';
+    import latex from 'highlight.js/lib/languages/latex';
+    import markdown from 'highlight.js/lib/languages/markdown';
+    import json from 'highlight.js/lib/languages/json';
+
+    hljs.registerLanguage('javascript', javascript);
+    hljs.registerLanguage('html', xml);
+    hljs.registerLanguage('sh', shell);
+    hljs.registerLanguage('md', markdown);
+    hljs.registerLanguage('latex', latex);
+    hljs.registerLanguage('json', json);
 
     export let content: string;
     export let language: string;
 
-    const result = hljs.highlight(content, { language: language ?? 'shell' });
+    const result = hljs.highlight(content, { language: language ?? 'sh' });
 </script>
+
 <!-- eslint-disable-next-line svelte/no-at-html-tags -->
 <pre><code class={`language-${language}`}>{@html result.value}</code></pre>
