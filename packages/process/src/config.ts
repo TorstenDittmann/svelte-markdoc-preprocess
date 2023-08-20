c775193ab84515e9b5ea9b7537f342062cc4f50f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-08-20 11:14:41 +0200

    chore: add some comments

--- packages/process/src/config.ts
+++ packages/process/src/config.ts
@@ -1,11 +1,23 @@
-import { SvelteComponent } from 'svelte';
-import { NodeType } from '@markdoc/markdoc';
-
 export type Config = {
+    /**
+     * Extensions to be processed.
+     */
     extensions: string[];
+    /**
+     * Absoulute path to the .svelte file exporting components for nodes.
+     */
     nodes: string | null;
+    /**
+     * Absoulute path to the .svelte file exporting components for tags.
+     */
     tags: string | null;
+    /**
+     * Generate schema files under `./svelte-kit/markdoc-schema.json` to be used with the official [Visual Studio Code extension](https://marketplace.visualstudio.com/items?itemName=Stripe.markdoc-language-support).
+     */
     generateSchema: boolean;
+    /**
+     * Layouts to be used for pages.
+     */
     layouts: {
         default: string;
         [key: string]: string;
