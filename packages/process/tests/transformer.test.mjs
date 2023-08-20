133ba8fb7555fabec0200389bf67145f41db4161
Torsten Dittmann <torsten.dittmann@googlemail.com>
2023-08-20 12:21:28 +0200

    tests: remove outdated test

--- packages/process/tests/transformer.test.mjs
+++ packages/process/tests/transformer.test.mjs
@@ -1,52 +1,6 @@
 import { test } from 'node:test';
 import assert from 'node:assert/strict';
-import { sanitize_for_svelte, ts_to_type } from '../dist/transformer.js';
-
-test('tsToType', async (context) => {
-    const node = {
-        type: 'BindingIdentifier',
-    };
-    await context.test('handles strings', () => {
-        const type = ts_to_type({
-            ...node,
-            typeAnnotation: {
-                typeAnnotation: {
-                    type: 'TsKeywordType',
-                    kind: 'string',
-                },
-            },
-        });
-        assert.equal(type, String);
-    });
-    await context.test('handles numbers', () => {
-        const type = ts_to_type({
-            ...node,
-            typeAnnotation: {
-                typeAnnotation: {
-                    type: 'TsKeywordType',
-                    kind: 'number',
-                },
-            },
-        });
-        assert.equal(type, Number);
-    });
-    await context.test('handles boolean', () => {
-        const type = ts_to_type({
-            ...node,
-            typeAnnotation: {
-                typeAnnotation: {
-                    type: 'TsKeywordType',
-                    kind: 'boolean',
-                },
-            },
-        });
-        assert.equal(type, Boolean);
-    });
-    await context.test('falls back to string', () => {
-        const type = ts_to_type(node);
-        assert.equal(type, String);
-    });
-});
+import { sanitize_for_svelte } from '../dist/transformer.js';
 
 test('sanitize for svelte', async (context) => {
     await Promise.all(
