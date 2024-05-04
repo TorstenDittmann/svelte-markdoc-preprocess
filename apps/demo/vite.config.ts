be819a83f29bca06ce0eb36c76e69f48f6bdd364
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-05-04 19:19:35 +0200

    fix: linter

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [enhancedImages(), sveltekit()],
+    plugins: [sveltekit()],
 });
