7712e0ac0c03610a5a6f4c8218fdcfdfff8ad559
Torsten Dittmann <torsten.dittmann@googlemail.com>
2026-02-08 23:42:42 +0400

    Migrate docs app to Docia

---
+++ apps/documentation/docia.config.ts
@@ -0,0 +1,21 @@
+export default {
+    srcDir: 'docs',
+    outDir: 'doc_build',
+    publicDir: 'docs/public',
+    basePath: '/',
+    prettyUrls: true,
+    site: {
+        title: 'Svelte Markdoc',
+        description:
+            'Bring the power of Markdoc right into your Svelte applications!',
+        language: 'en',
+        url: 'https://svelte-markdoc-preprocess.pages.dev',
+        socials: {
+            github:
+                'https://github.com/TorstenDittmann/svelte-markdoc-preprocess',
+            x: 'https://x.com/DittmannTorsten',
+        },
+        githubEditBaseUrl:
+            'https://github.com/TorstenDittmann/svelte-markdoc-preprocess/edit/main/apps/documentation/docs',
+    },
+};
